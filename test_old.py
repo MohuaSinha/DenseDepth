ff0c319d58d7a5732851758bd602da3dafc425a0
MSMarch2018 <37033968+MSMarch2018@users.noreply.github.com>
2020-05-08 00:16:12 +0530

    modify test for S15A

--- test_old.py
+++ test_old.py
@@ -13,10 +13,7 @@ from matplotlib import pyplot as plt
 # Argument Parser
 parser = argparse.ArgumentParser(description='High Quality Monocular Depth Estimation via Transfer Learning')
 parser.add_argument('--model', default='nyu.h5', type=str, help='Trained Keras model file.')
-parser.add_argument('--input', default='examples/*.jpg', type=str, help='Input filename or folder.')
-parser.add_argument('--start', default=1, type=str, help='start of file name.')
-parser.add_argument('--end', default=11, type=str, help='end-1 of file name.')
-
+parser.add_argument('--input', default='examples/*.png', type=str, help='Input filename or folder.')
 args = parser.parse_args()
 
 # Custom object needed for inference and training
@@ -40,9 +37,8 @@ outputs = predict(model, inputs)
 #matplotlib.use('TkAgg')   
 
 # Display results
-display_images(outputs.copy(), inputs.copy(),start = int(args.start), end = int(args.end))
-# plt.figure(figsize=(1,1))
-# plt.imshow(viz)
-# plt.savefig('test.png')
-# plt.show()
-print("done")
+viz = display_images(outputs.copy(), inputs.copy())
+plt.figure(figsize=(10,5))
+plt.imshow(viz)
+plt.savefig('test.png')
+plt.show()
